7ad57eb074c9db11fcd02cf5b07a1380b56d995c
Istvan Viczian <viczian.istvan@gmail.com>
2018-11-23 15:42:55 +0100

    No wait

--- ep-bva/deploy.sh
+++ ep-bva/deploy.sh
@@ -2,14 +2,6 @@
 
 ssh developer@tadev.training360.com '/opt/training/epbva/shutdown.sh'
 
-echo Waiting for shutdown
-
-ssh developer@tadev.training360.com 'grep -m 1 "shutdown" <(tail -f /opt/training/epbva/logs/console.log)'
-
 scp target/ep-bva-0.0.1-SNAPSHOT.jar developer@tadev.training360.com:/opt/training/epbva
 
 ssh developer@tadev.training360.com '/opt/training/epbva/startup.sh'
-
-echo Waiting for startup
-
-ssh developer@tadev.training360.com 'grep -m 1 "Started EpBvaApplication" <(tail -f /opt/training/epbva/logs/console.log)'
